5691dbe1441709b67ff2dd3448f6ae681aa0e1a8
Lando B <jrolandobernal@gmail.com>
2015-10-28 21:24:23 -0500

    Added classes BoardContext, BoardService, and added Unit Tests for those classes.

---
+++ Brello/Models/BoardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Brello.Models
+{
+    public class BoardContext : DbContext
+    {
+
+        // We use this to create our tables, and then get and set data from those tables
+        public virtual IDbSet<Color> Colors { get; set; }
+        public virtual IDbSet<Card> Cards { get; set; }
+
+    }
+
+    
+}
--- Brello/Models/Color.cs
+++ Brello/Models/Color.cs
@@ -5,10 +5,48 @@ using System.Web;
 
 namespace Brello.Models
 {
-    public class Color
+    public class Color : IComparable
     {
         public int ColorId { get; }
         public string Name { get; set; }
         public string Value { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Color other_color = obj as Color;
+            // Other way to cast is:
+            // Color other_color = (Color)obj;
+            return this.Name.CompareTo(other_color.Name);
+            /*
+            if (this.Name == other_color.Name) {
+                return 0;
+            }
+            else if (this.Name < other_color.Name)
+            {
+                return -1;
+            }
+            // throw new NotImplementedException();
+            */
+        }
+
+        public static bool operator ==(Color color1, object obj2)
+        {
+            return 0 == color1.CompareTo(obj2 as Color);
+        }
+
+        public static bool operator !=(Color color1, object obj2)
+        {
+            return 0 != color1.CompareTo(obj2 as Color); ;
+        }
+
+        public static bool operator >(Color color1, object obj2)
+        {
+            return 1 == color1.CompareTo(obj2 as Color);
+        }
+
+        public static bool operator <(Color color1, object obj2)
+        {
+            return -1 == color1.CompareTo(obj2 as Color);
+        }
     }
 }
