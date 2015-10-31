61c74bbe92cd11bbf7d6fa26fe3270439e260a48
Lando B <jrolandobernal@gmail.com>
2015-10-31 14:00:18 -0500

    Basic tests and Board class added

---
+++ Brello/Models/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Brello.Models
+{
+    public class Board
+    {
+        public int BoardId { get; set; }
+        public int Title { get; set; }
+
+        // Assigned To
+        public virtual ICollection<BrelloList> Lists { get; set; }
+        // Vote mechanism
+        public virtual ICollection<ApplicationUser> Followers { get; set; }
+    }
+}
