517ef760f394e9ed20b2cc0ff21cdc330bea6732
Lando B <jrolandobernal@gmail.com>
2015-12-10 07:31:46 -0600

    Added AngularJS. Added API call to get JSON

---
+++ Brello.Tests/Controllers/BoardApiControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Controllers;
+using System.Web.Mvc;
+using System.Web.Http.Results;
+
+namespace Brello.Tests.Controllers
+{
+    [TestClass]
+    public class BoardApiControllerTests
+    {
+
+        [TestMethod]
+        public void BoardApiEnsureICanCallGetMethod()
+        {
+            //Begin Arrange
+            BoardApiController my_controller = new BoardApiController();
+            string expected_output = "Hello, is it me you're looking for?";
+            //End Arrange
+
+            //Begin Act
+            JsonResult<string> actual_output = my_controller.Get();
+            //End Act
+
+            //Begin Assert
+            Assert.AreEqual(expected_output, actual_output);
+            //End Assert
+        }
+
+    }
+}
